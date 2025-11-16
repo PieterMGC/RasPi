077ef0a8a540b1a22ca371a765529393f4b69ce8
PieterMGC <pieter.ceuppens@gmail.com>
2025-11-16 15:49:49 +0100

    auto

--- P-McWo-Training/L41-RPiServer.py
+++ P-McWo-Training/L41-RPiServer.py
@@ -4,6 +4,7 @@ from time import sleep
 bufferSize = 1024
 serverPort = 2222
 serverIP = "192.168.86.35"
+counter = 0
 
 msgFromServer = "Hello client, I am your server."
 bytesToSend = msgFromServer.encode('utf-8')
@@ -11,8 +12,15 @@ bytesToSend = msgFromServer.encode('utf-8')
 RPISocket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 RPISocket.bind((serverIP,serverPort))
 print("Server is up and listening...")
-message,address = RPISocket.recvfrom(bufferSize)
-message = message.decode('utf-8')
-print(message)
-print('Client Adress: ',address[0])
-RPISocket.sendto(bytesToSend,address)
+while True:
+    message,address = RPISocket.recvfrom(bufferSize)
+    message = message.decode('utf-8')
+    print(message)
+    print('Client Adress: ',address[0])
+    if message == 'INC':
+        counter += 1
+    elif message == 'DEC':
+        counter -= 1
+    msg = str(counter)
+    msg = msg.encode('utf-8')
+    RPISocket.sendto(msg,address)
