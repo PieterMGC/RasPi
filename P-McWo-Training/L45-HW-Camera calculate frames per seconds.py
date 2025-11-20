f3c0a1f6707f3bb56842711dc86d0fb8a282d9d3
PieterMGC <pieter.ceuppens@gmail.com>
2025-11-20 15:31:45 +0100

    auto

--- P-McWo-Training/L45-HW-Camera calculate frames per seconds.py
+++ P-McWo-Training/L45-HW-Camera calculate frames per seconds.py
@@ -1,24 +1,29 @@
+#calculate frames per seconds
 import cv2
 print(cv2.__version__)
 import time
 from picamera2 import Picamera2
 
-count = 0
-
 picam = Picamera2()
 picam.preview_configuration.main.size = (1280,720)
 picam.preview_configuration.main.format = "RGB888"
-picam.preview_configuration.controls.FrameRate = 60
+picam.preview_configuration.controls.FrameRate = 30
 picam.preview_configuration.align()
 picam.configure("preview")
 picam.start()
 
 while True:
-    im = picam.capture_array()
-    cv2.imshow("Camera", im)
+    count = 0
+    start = time.time()
+    while time.time() - start < 1:
+        im = picam.capture_array()
+        count += 1
+        cv2.imshow("Camera", im)
+    print (count)
     if cv2.waitKey(1) == ord('q'):
         break
 picam.stop()
 picam.close()
 cv2.destroyAllWindows()
 print("All is closed")
+
