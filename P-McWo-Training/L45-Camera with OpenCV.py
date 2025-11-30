cd13b2c029f4ad93fde7825b0e80ba38cbfa0f36
PieterMGC <pieter.ceuppens@gmail.com>
2025-11-30 13:36:36 +0100

    auto

--- P-McWo-Training/L45-Camera with OpenCV.py
+++ P-McWo-Training/L45-Camera with OpenCV.py
@@ -3,8 +3,11 @@ print(cv2.__version__)
 import time
 from picamera2 import Picamera2
 
+displ_w = 1280
+displ_h = 720
+
 picam = Picamera2()
-picam.preview_configuration.main.size = (1280,720)
+picam.preview_configuration.main.size = (displ_w,displ_h)
 picam.preview_configuration.main.format = "RGB888"
 picam.preview_configuration.controls.FrameRate = 60
 picam.preview_configuration.align()
--- P-McWo-Training/L50-Camera modify image and create region of interest.py
+++ P-McWo-Training/L50-Camera modify image and create region of interest.py
@@ -3,8 +3,11 @@ print(cv2.__version__)
 import time
 from picamera2 import Picamera2
 
+displ_w = 1280
+displ_h = 720
+
 picam = Picamera2()
-picam.preview_configuration.main.size = (1280,720)
+picam.preview_configuration.main.size = (displ_w,displ_h)
 picam.preview_configuration.main.format = "RGB888"
 picam.preview_configuration.controls.FrameRate = 60
 picam.preview_configuration.align()
@@ -13,7 +16,9 @@ picam.start()
 
 while True:
     im = picam.capture_array()
+    roi = im[0:int(displ_h/2), 0:int(displ_w/2)]
     cv2.imshow("Camera", im)
+    cv2.imshow("ROI", roi)
     if cv2.waitKey(1) == ord('q'):
         break
 picam.stop()
